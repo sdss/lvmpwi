311bc5606565c7fd4e8b872df9341c99d7e5f6d5
Florian Briegel <wasndas@zone42.de>
2021-08-10 22:00:36 +0200

    add lvmt/data for persistent container data from pwi

--- container/run-pwi.sh
+++ container/run-pwi.sh
@@ -9,7 +9,7 @@ setup_pwi4() {
     (cd ~/PlaneWave\ Instruments/PWI4/ && ln -s $LVMT_CONFIG_PATH/PWI4/Settings/ )
     (cd ~/PlaneWave\ Instruments/ && ln -s $LVMT_CONFIG_PATH/PWI4/Mount\ Tuning/ )
     mkdir -p $LVMT_PATH/data
-    (cd $PWI_PATH && ln -sf $LVMT_PATH/data/ data/ )
+    (cd $PWI_PATH && ln -sf $LVMT_PATH/data data )
 }
 
 start_pwi4() {
