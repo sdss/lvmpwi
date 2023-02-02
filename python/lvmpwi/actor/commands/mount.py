b877d0d6efc081ed7c0450264de56754cd57ab98
Florian Briegel <wasndas@zone42.de>
2023-02-02 11:01:15 +0100

    made status update every 1 sec defult

--- python/lvmpwi/actor/commands/mount.py
+++ python/lvmpwi/actor/commands/mount.py
@@ -60,7 +60,7 @@ async def statusTick(command, pwi: PWI4, delta_time):
 
 @parser.command("setConnected")
 @click.argument("enable", type=bool)
-@click.option("--statusTick", type=float, default=-1)
+@click.option("--statusTick", type=float, default=1)
 async def setConnected(command: Command, pwi: PWI4, enable:bool, statustick:float):
     """set mount connected true/false """
 
