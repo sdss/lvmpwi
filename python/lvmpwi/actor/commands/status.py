75e3a94e28dd397863189e8b8ddbe128566ae994
José Sánchez-Gallego <gallegoj@uw.edu>
2024-02-04 13:12:36 -0800

    Fix axis0/1 position_timestamp_str in status command

--- python/lvmpwi/actor/commands/status.py
+++ python/lvmpwi/actor/commands/status.py
@@ -25,7 +25,7 @@ async def status(command: Command, pwi: PWI4):
             is_connected=status.mount.is_connected,
             is_slewing=status.mount.is_slewing,
             is_enabled=status.mount.axis0.is_enabled & status.mount.axis1.is_enabled,
-            
+
             ra_j2000_hours=status.mount.ra_j2000_hours,
             dec_j2000_degs=status.mount.dec_j2000_degs,
 
@@ -45,6 +45,7 @@ async def status(command: Command, pwi: PWI4):
                 'position_degs': status.mount.axis0.position_degs,
                 'rms_error_arcsec': status.mount.axis0.rms_error_arcsec,
                 'servo_error_arcsec': status.mount.axis0.servo_error_arcsec,
+                'position_timestamp': status.mount.axis0.position_timestamp_str,
             },
 
             axis1 = {
@@ -53,21 +54,17 @@ async def status(command: Command, pwi: PWI4):
                 'position_degs': status.mount.axis1.position_degs,
                 'rms_error_arcsec': status.mount.axis1.rms_error_arcsec,
                 'servo_error_arcsec': status.mount.axis1.servo_error_arcsec,
-                'position_degs': status.mount.axis0.position_degs,
-                'position_timestamp': status.mount.axis0.position_timestamp_str,
+                'position_timestamp': status.mount.axis1.position_timestamp_str,
             },
             model = {
                 'filename': status.mount.model.filename,
                 'num_points_enabled': status.mount.model.num_points_enabled,
                 'num_points_total': status.mount.model.num_points_total,
                 'rms_error_arcsec': status.mount.model.rms_error_arcsec,
-                'position_degs': status.mount.axis1.position_degs,
-                'position_timestamp': status.mount.axis1.position_timestamp_str,
             },
 
             geometry=status.mount.geometry,
         )
-            
+
     except Exception as ex:
         return command.fail(error=ex)
-
